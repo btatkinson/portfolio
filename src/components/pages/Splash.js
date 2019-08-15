3975af98ef71abfdbe4fc4b37d40ae95ebe0c4c9
btatkinson <blaketatkinson@gmail.com>
2019-08-15 07:19:31 -0400

    security update

--- src/components/pages/Splash.js
+++ src/components/pages/Splash.js
@@ -1,9 +1,11 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCircle } from '@fortawesome/free-solid-svg-icons';
 import '../../styles/Splash.scss';
 
 class Splash extends Component {
-  
+
   constructor(props) {
     super(props);
   }
@@ -14,14 +16,12 @@ class Splash extends Component {
         <div className='sp-container'>
           <div className='sp-header'>
             <h1>Blake Atkinson</h1>
-            <h4>Web Design and Data Science</h4>
+            <h4>Data Science • Front End Web Development</h4>
           </div>
-          <div className='spr-box'>
-            <Link to='/portfolio'>
-            Portfolio</Link>
-          </div>
-          <div className='spp-box'>
-            <Link to='/resume'>Resume</Link>
+          <div className='sp-body'>
+            <Link className='sp-link' to='/portfolio'><h1 className='bump-hover'>Portfolio</h1></Link>
+            <Link className='sp-link' to='/resume'><h1 className='bump-hover'>Resume</h1></Link>
+            <Link className='sp-link' to='/contact'><h1 className='bump-hover'>Contact</h1></Link>
           </div>
         </div>
       </div>
